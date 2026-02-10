3abca4032e5e4ae8247cc0e76dbaa7d8ce731991
YedLeo1 <121866890+YedLeo1@users.noreply.github.com>
2026-02-10 08:02:07 +0800

    Update tz_tee_call.c

--- tz_tee_call.c
+++ tz_tee_call.c
@@ -1,7 +1,13 @@
 #include <linux/module.h>
 
-static int __init tz_init(void) { return 0; }
-static void __exit tz_exit(void) {}
+static int __init tz_init(void) {
+    pr_info("tz_tee_call: loaded\n");
+    return 0;
+}
+
+static void __exit tz_exit(void) {
+    pr_info("tz_tee_call: unloaded\n");
+}
 
 module_init(tz_init);
 module_exit(tz_exit);
